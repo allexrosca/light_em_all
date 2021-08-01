826e3a8632a07c8ef467df76d2283fdbf78c65af
Alex Rosca <alexandru.rosca@yahoo.com>
2021-08-01 14:49:09 +0300

    code refactoring

--- light_em_all/src/light_sens/light_sens.cpp
+++ light_em_all/src/light_sens/light_sens.cpp
@@ -1,16 +1,16 @@
 #include "light_sens.h"
 
-void light_sens_init(const int light_sens_pin){
-    pinMode(light_sens_pin, INPUT);  
+void light_sens_init(const int light_sens_pin) {
+    pinMode(light_sens_pin, INPUT);
 }
 
-int light_sens_run(const int light_sens_pin){
+int light_sens_run(const int light_sens_pin) {
     // 0 -> light; 1 -> no light
-    return digitalRead(light_sens_pin); 
+    return digitalRead(light_sens_pin);
 }
 
 
-bool light_sens_check_if_connected(const int light_sens_pin){
+bool light_sens_check_if_connected(const int light_sens_pin) {
     bool firstCheck = light_sens_run(light_sens_pin);
     delay(500);
     bool secondCheck = light_sens_run(light_sens_pin);
